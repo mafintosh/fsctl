474cea95320728864c3b1688946736a73a3c4320
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-05-06 11:00:07 +0200

    Implement hole punching on Linux

---
+++ src/linux.c
@@ -0,0 +1,14 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <uv.h>
+
+#include "../include/fsctl.h"
+#include "platform.h"
+
+int
+fsctl__punch_hole (uv_os_fd_t fd, uint64_t offset, size_t length) {
+  int res = fallocate(fd, FALLOC_FL_PUNCH_HOLE | FALLOC_FL_KEEP_SIZE, offset, length);
+
+  return res == -1 ? uv_translate_sys_error(errno) : res;
+}
