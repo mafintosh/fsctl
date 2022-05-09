93173af0c065ab566de89c82637076aec2784960
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-05-09 11:35:32 +0200

    Add some diagnostic comment to punch test

--- test/punch-hole.mjs
+++ test/punch-hole.mjs
@@ -6,16 +6,23 @@ import { punchHole, setSparse } from '../index.js'
 test('punch hole', async (t) => {
   const file = await open('test/fixture/sparse.txt', 'w+')
 
-  const empty = 1024 * 1024 * 10 // 10 MB
+  const empty = Buffer.alloc(1024 * 1024 * 10) // 10 MB
+  await file.write(empty)
 
   const write = Buffer.from('hello world')
-  await file.write(write, 0, 11, empty)
+  await file.write(write)
+
+  const { blocks: blocksBefore } = await file.stat()
+  t.comment('blocks before: ' + blocksBefore)
 
   setSparse(file.fd)
-  await punchHole(file.fd, 0, empty)
+  await punchHole(file.fd, 0, empty.byteLength)
+
+  const { blocks: blocksAfter } = await file.stat()
+  t.comment('blocks after: ' + blocksAfter)
 
   const read = Buffer.alloc(11)
-  await file.read(read, 0, 11, empty)
+  await file.read(read, 0, 11, empty.byteLength)
 
-  t.alike(read, write)
+  t.alike(read, write, 'file is intact')
 })
