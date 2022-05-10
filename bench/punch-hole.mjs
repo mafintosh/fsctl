27a24b422e0a18b06a60efe4dbbeaf610484e50d
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-05-10 14:23:30 +0200

    Add hole punching benchmark

---
+++ bench/punch-hole.mjs
@@ -0,0 +1,20 @@
+import bench from 'nanobench'
+import { open } from 'fs/promises'
+import prettyBytes from 'pretty-bytes'
+import { punchHole } from '../index.js'
+
+for (let i = 12; i < 31; i++) {
+  const n = 2 ** i
+
+  bench(`punching ${prettyBytes(n)}`, async function (b) {
+    const file = await open(`bench/data/sparse-${n}.txt`, 'w+')
+
+    file.write(Buffer.alloc(n))
+
+    b.start()
+    await punchHole(file.fd, 0, n)
+    b.end()
+
+    await file.close()
+  })
+}
