9ceba782d63e95e579ffdf10f147796b3804652e
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-06-10 13:10:20 +0200

    Use BSD locks on Linux for now

--- src/linux.c
+++ src/linux.c
@@ -9,51 +9,6 @@
 #include "platform.h"
 
 int
-fsctl__lock (uv_os_fd_t fd, uint64_t offset, size_t length, fsctl_lock_type_t type) {
-  struct flock data = {
-    .l_start = offset,
-    .l_len = length,
-    .l_pid = 0,
-    .l_type = type == FSCTL_WRLOCK ? F_WRLCK : F_RDLCK,
-    .l_whence = SEEK_SET,
-  };
-
-  int res = fcntl(fd, F_OFD_SETLKW, &data);
-
-  return res == -1 ? uv_translate_sys_error(errno) : res;
-}
-
-int
-fsctl__try_lock (uv_os_fd_t fd, uint64_t offset, size_t length, fsctl_lock_type_t type) {
-  struct flock data = {
-    .l_start = offset,
-    .l_len = length,
-    .l_pid = 0,
-    .l_type = type == FSCTL_WRLOCK ? F_WRLCK : F_RDLCK,
-    .l_whence = SEEK_SET,
-  };
-
-  int res = fcntl(fd, F_OFD_SETLK, &data);
-
-  return res == -1 ? uv_translate_sys_error(errno) : res;
-}
-
-int
-fsctl__unlock (uv_os_fd_t fd, uint64_t offset, size_t length) {
-  struct flock data = {
-    .l_start = offset,
-    .l_len = length,
-    .l_pid = 0,
-    .l_type = F_UNLCK,
-    .l_whence = SEEK_SET,
-  };
-
-  int res = fcntl(fd, F_OFD_SETLK, &data);
-
-  return res == -1 ? uv_translate_sys_error(errno) : res;
-}
-
-int
 fsctl__punch_hole (uv_os_fd_t fd, uint64_t offset, size_t length) {
   int res = fallocate(fd, FALLOC_FL_PUNCH_HOLE | FALLOC_FL_KEEP_SIZE, offset, length);
 
--- src/mac.c
+++ src/mac.c
@@ -7,33 +7,6 @@
 #include "platform.h"
 
 int
-fsctl__lock (uv_os_fd_t fd, uint64_t offset, size_t length, fsctl_lock_type_t type) {
-  if (offset != 0 || length != 0) return UV_EINVAL;
-
-  int res = flock(fd, type == FSCTL_WRLOCK ? LOCK_EX : LOCK_SH);
-
-  return res == -1 ? uv_translate_sys_error(errno) : res;
-}
-
-int
-fsctl__try_lock (uv_os_fd_t fd, uint64_t offset, size_t length, fsctl_lock_type_t type) {
-  if (offset != 0 || length != 0) return UV_EINVAL;
-
-  int res = flock(fd, (type == FSCTL_WRLOCK ? LOCK_EX : LOCK_SH) | LOCK_NB);
-
-  return res == -1 ? uv_translate_sys_error(errno) : res;
-}
-
-int
-fsctl__unlock (uv_os_fd_t fd, uint64_t offset, size_t length) {
-  if (offset != 0 || length != 0) return UV_EINVAL;
-
-  int res = flock(fd, LOCK_UN);
-
-  return res == -1 ? uv_translate_sys_error(errno) : res;
-}
-
-int
 fsctl__punch_hole (uv_os_fd_t fd, uint64_t offset, size_t length) {
   struct fpunchhole data = {
     .fp_flags = 0,
